3f375e733e1f1f90c57f5b59eda94cdb3399aefa
Neuro122 <jeppeetzerodt@gmail.com>
2020-03-18 18:43:15 +0100

    firetrap swinger

---
+++ Assets/Scripts/LavaTrap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaTrap : MonoBehaviour
+{
+	
+
+	private GameObject penguinManagerGameObject;
+	private PenguinManager penguinManagerComponent;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		penguinManagerGameObject = GameObject.FindWithTag("PenguinManager");
+		penguinManagerComponent = penguinManagerGameObject.GetComponent<PenguinManager>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if(collision.gameObject.CompareTag("Penguin"))
+		{
+			penguinManagerComponent.CountPenguin(1, true);
+		}
+	}
+}
